a7298cf74bd7cbfc7f72d0302b51622dee78f692
Pillow's Tiny Desktop <109895040+ModulePillow@users.noreply.github.com>
2026-09-17 21:50:13 +0800

    Add two experiment files.

---
+++ SourceCode/Crucible/Core/Physics.cs
@@ -0,0 +1,30 @@
+using SharpDX;
+
+namespace Crucible;
+
+// 2026-9-17: Disable this experiment file.
+#if DISABLED
+public static class Physics
+{
+  static bool BoxContainsPoint(ref BoundingBox rect, ref Vector3 dot)
+  {
+    var result = SharpDX.Collision.BoxContainsPoint(ref rect, ref dot);
+    return result == ContainmentType.Disjoint ? false : true;
+  }
+
+  static bool LineTriangleIntersection(ref Ray ray, ref Vector3 v1, ref Vector3 v2, ref Vector3 v3, out float distance)
+  {
+    return SharpDX.Collision.RayIntersectsTriangle(ref ray, ref v1, ref v2, ref v3, out distance);
+  }
+
+  static bool LineTriangleIntersection(ref Ray ray, ref Vector3 v1, ref Vector3 v2, ref Vector3 v3, out Vector3 pos)
+  {
+    return SharpDX.Collision.RayIntersectsTriangle(ref ray, ref v1, ref v2, ref v3, out pos);
+  }
+
+  static void a()
+  {
+    //SharpDX.Collision.
+  }
+}
+#endif
