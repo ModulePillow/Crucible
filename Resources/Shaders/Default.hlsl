125593b35c3fe2125cb1c72b35852f99cd2fd10d
Pillow's Tiny Desktop <109895040+ModulePillow@users.noreply.github.com>
2026-09-18 09:57:20 +0800

    HLSL cleanup

--- Resources/Shaders/Default.hlsl
+++ Resources/Shaders/Default.hlsl
@@ -1,23 +1,25 @@
 #include "Common.hlsl"
 
+void TempStippleEffect(float2 NDCCoord, float alpha);
+
 Texture2D albedo : register(t0);
 
 VertexOut VS(VertexIn vin)
 {
-  VertexOut vout;
+   VertexOut vout;
   // Transform to homogeneous clip space.
-  float4 posW = mul(float4(vin.pL, 1.0f), mtxW);
-  vout.pos = mul(posW, mtxVP);
-  vout.normal = normalize(vin.nL);
-  vout.tangent = normalize(vin.tL);
-  vout.uv = vin.uv;
-  return vout;
+   float4 posW = mul(float4(vin.pL, 1.0f), mtxW);
+   vout.pos = mul(posW, mtxVP);
+   vout.normal = normalize(mul(vin.nL, (float3x3)mtxW));
+   vout.tangent = normalize(mul(vin.tL, (float3x3) mtxW));
+   vout.uv = vin.uv;
+   return vout;
 }
 
 float4 PS(VertexOut pin) : SV_Target
 {
-  float4 output = 0;
-  float4 color = albedo.Sample(Ani, pin.uv);
+   float4 output = 0;
+   float4 color = albedo.Sample(Ani, pin.uv);
   //normal_m.xyz = GetWorldNormal(normal_m.xyz, 0, 0);
 
   // spec/diff/reflectvitity are all generated from metallic.
@@ -28,44 +30,42 @@ float4 PS(VertexOut pin) : SV_Target
   //float oneMinusReflectivity = 0.96 - normal_m.a * 0.96;
   //float3 diffColor = albedo_s.rgb * oneMinusReflectivity;
 
-  float3 giColor = 0;
-  float3 emissiveColor = 0;
-  float3 extraColor = giColor + emissiveColor;
+   float3 giColor = 0;
+   float3 emissiveColor = 0;
+   float3 extraColor = giColor + emissiveColor;
 
   //output.backBuffer = float4(extraColor, 0);
   //output.backBuffer = float4(EncodeSRGB(albedo_s).rgb, 0);
-  output = EncodeSRGB(color);//EncodeSRGB(normal_m);//float4(pin.normal, 0);
+   output = EncodeSRGB(color); //EncodeSRGB(normal_m);//float4(pin.normal, 0);
 
-  // TEMP Light
-  float3 lightDir = normalize(float3(0.5, -2, 0.5));
-  float3 normal = normalize(pin.normal);
-  normal.y *= -1;
-  float factor = clamp(0.5 * dot(lightDir, normal) + 0.5 + 0.1, 0, 1);
-  output *= factor;
-  ////////
+   // TEMP Light
+   float3 lightDir = normalize(float3(0, -1, 0));
+   float3 normal = normalize(pin.normal);
+   float factor = clamp(0.5 * dot(-lightDir, normal) + 0.5 + 0.1, 0, 1);
+   output *= factor;
 
-  // TEMP Stipple Effect
-  float alphaHC = 1.0f;
-  //float alphaHC = 0.2f;
-  int2 screenSizeHC = int2(1920, 1080);
-  // Thresholds
-  float stipple[4][4] =
-  {
-    { 0.0/16,  8.0/16,  2.0/16, 10.0/16},
-    {12.0/16,  4.0/16, 14.0/16,  6.0/16},
-    { 3.0/16, 11.0/16,  1.0/16,  9.0/16},
-    {15.0/16,  7.0/16, 13.0/16,  5.0/16}
-  };
+   //TempStippleEffect(pin.pos.xy, 0.2f);
+   //TempStippleEffect(pin.pos.xy, 1.0f);
 
-  int2 screenPos = floor(pin.pos.xy);
+   return output;
+}
 
-  int2 stipplePos = int2(screenPos.x % 4, screenPos.y % 4);
-  float stippleValue = stipple[stipplePos.x][stipplePos.y];
-  if(alphaHC < stippleValue)
-  {
-    discard;
-  }
-  ////////
+void TempStippleEffect(float2 NDCCoord, float alpha)
+{
+   const int2 screenSizeHC = int2(1920, 1080);
+   const float stipple[4][4] = // Thresholds
+   {
+      { 0.0 / 16, 8.0 / 16, 2.0 / 16, 10.0 / 16 },
+      { 12.0 / 16, 4.0 / 16, 14.0 / 16, 6.0 / 16 },
+      { 3.0 / 16, 11.0 / 16, 1.0 / 16, 9.0 / 16 },
+      { 15.0 / 16, 7.0 / 16, 13.0 / 16, 5.0 / 16 }
+   };
+   int2 screenPos = floor(NDCCoord);
+   int2 stipplePos = int2(screenPos.x % 4, screenPos.y % 4);
+   float stippleValue = stipple[stipplePos.x][stipplePos.y];
 
-  return output;
+   if (alpha < stippleValue)
+   {
+      discard;
+   }
 }
